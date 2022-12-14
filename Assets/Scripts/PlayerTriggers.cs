2025eb7c39d3d696ce118975d404db0fb2208cb7
leonorv <leonor.veloso@tecnico.ulisboa.pt>
2022-12-14 15:44:36 +0100

    added npcs and changed jump and run location

--- Assets/Scripts/PlayerTriggers.cs
+++ Assets/Scripts/PlayerTriggers.cs
@@ -10,15 +10,28 @@ public class PlayerTriggers : MonoBehaviour
     public GameObject dialogueObject;
     private void OnTriggerStay2D(Collider2D col)
     {
+        if (col.gameObject.name == "StartJmpNRunMinigame")
+            {
+                SceneManager.LoadScene("JumpAndRun", LoadSceneMode.Single);
+            }
+
         if (Input.GetKeyDown("f"))
         {
             if (col.gameObject.name == "StartTurbineMinigame")
             {
                 SceneManager.LoadScene("BuildAWindTurbine", LoadSceneMode.Single);
             }
-            else if (col.gameObject.name == "StartJmpNRunMinigame")
+            else if (col.gameObject.name == "StartPathSign")
             {
-                SceneManager.LoadScene("JumpAndRun", LoadSceneMode.Single);
+                DialogueReader reader = new DialogueReader("pathdialogue.txt");
+                dialogueObject.GetComponent<DialogueDisplay>().SetDialogueReader(reader);
+                dialogueObject.GetComponent<DialogueDisplay>().DialogueUpdate();
+            }
+            else if (col.gameObject.name == "StartMayorDialogue")
+            {
+                DialogueReader reader = new DialogueReader("mayordialogue.txt");
+                dialogueObject.GetComponent<DialogueDisplay>().SetDialogueReader(reader);
+                dialogueObject.GetComponent<DialogueDisplay>().DialogueUpdate();
             }
         }
     }
