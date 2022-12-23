8f7bd7f43f1940c98a564bfe378d2815c505607e
Aaron Giner <aaron.giner@student.tugraz.at>
2022-12-23 13:20:26 +0100

    fixed usb slot resolution issue

--- Assets/Scripts/Missions/Sabotage/USB.cs
+++ Assets/Scripts/Missions/Sabotage/USB.cs
@@ -6,55 +6,55 @@ namespace Missions.Sabotage
     public class USB : MonoBehaviour
     {
         public GameObject game;
+        public GameObject canvas;
+        public GameObject usbSlot;
 
         private VirusGame _gameScript;
         private Rigidbody2D _rigidbody2D;
-        private Vector3 _positionStart;
-        private Quaternion _rotationStart;
         private Vector3 _clickPositionDiffVector;
+
+        private bool _clicked;
 
         private void Start()
         {
             _gameScript = game.GetComponent<VirusGame>();
             _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
-            _positionStart = transform.position;
-            _rotationStart = transform.rotation;
         }
 
-        public void Drag()
+        public void Update()
         {
-            if (Input.GetMouseButton(0))
-            {
-                gameObject.transform.position = Input.mousePosition - _clickPositionDiffVector;
-            }
+            if (!(Input.GetMouseButton(0) && _clicked)) return;
+            
+            gameObject.transform.position = Input.mousePosition - _clickPositionDiffVector;
         }
 
         public void Click()
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                _clickPositionDiffVector = Input.mousePosition - gameObject.transform.position;
-            }
+            if (!Input.GetMouseButtonDown(0)) return;
+            
+            _clickPositionDiffVector = Input.mousePosition - gameObject.transform.position;
+            _clicked = true;
         }
 
         public void Release()
         {
-            if (Input.GetMouseButtonUp(0))
+            if (!Input.GetMouseButtonUp(0)) return;
+            
+            if (Vector3.Distance(usbSlot.transform.position, gameObject.transform.position) < 60)
             {
-                if (Vector3.Distance(_positionStart, gameObject.transform.position) < 60)
-                {
-                    _rigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
-                    transform.position = _positionStart;
-                    transform.rotation = _rotationStart;
+                _rigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
+                transform.position = usbSlot.transform.position;
+                transform.rotation = usbSlot.transform.rotation;
 
-                    _gameScript.usbAttached = true;
-                }
-                else
-                {
-                    _rigidbody2D.constraints = RigidbodyConstraints2D.None;
-                    _gameScript.usbAttached = false;
-                }
+                _gameScript.usbAttached = true;
             }
+            else
+            {
+                _rigidbody2D.constraints = RigidbodyConstraints2D.None;
+                _gameScript.usbAttached = false;
+            }
+
+            _clicked = false;
         }
     }
 }
