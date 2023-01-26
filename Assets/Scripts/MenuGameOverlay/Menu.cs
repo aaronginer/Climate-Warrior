bd72031136e0e0f6da65e2e3cef4ea5346ff539e
leonorv <leonor.veloso@tecnico.ulisboa.pt>
2023-01-26 15:28:22 +0000

    added quest menu and new credits menu

--- Assets/Scripts/MenuGameOverlay/Menu.cs
+++ Assets/Scripts/MenuGameOverlay/Menu.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 namespace MenuGameOverlay
 {
     public class Menu : MonoBehaviour
     {
         public GameObject menuContainer;
+        public GameObject controlsLayout;
+        public TextMeshProUGUI controlsText;
 
         private bool _active;
+
+
     
         public void Start()
         {
             _active = false;
             menuContainer.SetActive(_active);
+            controlsLayout.SetActive(_active);
         }
 
         // Update is called once per frame
@@ -22,6 +29,9 @@ namespace MenuGameOverlay
             {
                 _active = !_active;
                 menuContainer.SetActive(_active);
+                if (_active == false) {
+                    controlsLayout.SetActive(false);
+                }
                 // Time.timeScale = _active ? 0 : 1;
             }
         }
@@ -43,6 +53,14 @@ namespace MenuGameOverlay
             GameStateManager.Instance.SaveToDisk();
             GameStateManager.Destroy();
             SceneManager.LoadScene("_preload");
+        }
+
+        public void Controls()
+        {
+            controlsText.text = "ESC -> close this menu\nWASD -> walk around\nQ -> open/close quest menu\nE -> open/close inventory\nspace bar -> advance dialogue\nleft click -> choose dialogue option";
+            controlsLayout.SetActive(true);
+
+            
         }
     }       
 }
